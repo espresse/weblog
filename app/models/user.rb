f0aa76b75935db9f5288e0c92cfc1c1e8a91977f
unknown <Lajfbuk@Lajfbuk-laptop.(none)>
2012-02-24 19:14:49 +0100

    added user spec & user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,14 @@ class User < ActiveRecord::Base
   attr_accessor :password
 
   has_many :posts
-  
-  validates_confirmation_of :password
 
+  validates_confirmation_of :password
+  validates :password, presence: true, length: { minimum: 6}, :on => :create
+
+  valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: valid_email_regex }, uniqueness: { case_sensitive: false }
+  validates :username,  presence: true, length: { maximum: 25 }, uniqueness: { case_sensitive: false }
+
+  default_scope :order => ('posts_count desc')
+  
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,76 @@
+require 'spec_helper'
+
+describe User do
+
+  before do
+    @user = User.new(username: "Example User", email: "user@example.com", 
+    				password: "foobar", password_confirmation: "foobar")
+  end
+
+  subject { @user }
+
+  it { should respond_to(:username) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_hash) }
+  it { should respond_to(:password_salt) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
+
+  describe "when username is not present" do
+    before { @user.username = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when username is too long" do
+    before { @user.username = "a" * 26 }
+    it { should_not be_valid }
+  end
+
+  describe "when email format is invalid" do
+    invalid_addresses =  %w[user@example,com user_at_example.com example.user@example. user]
+    invalid_addresses.each do |invalid_address|
+      before { @user.email = invalid_address }
+      it { should_not be_valid }
+    end
+  end
+
+  describe "when email format is valid" do
+    valid_addresses = %w[user@example.com USER@mail.example.com user.name@example.pl user+name@example.co.uk]
+    valid_addresses.each do |valid_address|
+      before { @user.email = valid_address }
+      it { should be_valid }
+    end
+  end
+
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+end
