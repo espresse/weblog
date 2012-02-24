883e28aca065ffbc596986f0d5712d5ff5aecb9f
unknown <Lajfbuk@Lajfbuk-laptop.(none)>
2012-02-24 20:37:54 +0100

    after destroying user - his posts should be destroyed as well

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   before_save :encrypt_password
 
-  has_many :posts
+  has_many :posts, :dependent => :destroy
 
   validates_confirmation_of :password
   validates :password, presence: true, length: { minimum: 6}, :on => :create
