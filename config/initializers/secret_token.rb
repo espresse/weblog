ed7c72419d2688ce5f1205d33c57efc0b8285d62
unknown <Lajfbuk@Lajfbuk-laptop.(none)>
2012-02-24 17:58:08 +0100

    setting up environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weblog::Application.config.secret_token = 'da0e944319d34f652822507149505a49e32915030f441a9e7c568dd1d1e93cd4940ed854156fb2b7afa35323ef631c55e1c3eaf9ad804bb711702d809da760b7'
